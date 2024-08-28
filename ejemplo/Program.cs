ac9649946698bbade69e5b4a21b2422ff0d3cffc
ChristopherMaldonado24 <saidreyes711@gmail.com>
2024-08-28 13:26:00 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("== Ventas variadas ==");
+            Console.WriteLine("1. Transistores");
+            Console.WriteLine("2. Capacitores");
+            Console.WriteLine("3. ");
+            Console.WriteLine("== Ventas variadas ==");
+            Console.WriteLine("== Ventas variadas ==");
+        }
+    }
+}
